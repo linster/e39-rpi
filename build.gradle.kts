5bfd1d9c48d2b12ccbdcbdd7b01edc00c06b8c1c
Stefan Martynkiw <linster247@gmail.com>
2024-12-02 15:57:30 -0500

    WIP moving to new gradle structure so that Kotlin 2.1 is happy, so I can use trixnity because classpath conflict nonsense.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
-import org.jetbrains.compose.compose
-import org.jetbrains.kotlin.daemon.common.configureDaemonJVMOptions
-
+plugins {
+    // this is necessary to avoid the plugins to be loaded multiple times
+    // in each subproject's classloader
+    alias(libs.plugins.composeMultiplatform) apply false
+    alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
+}
 
 buildscript {
     repositories {
@@ -20,38 +24,16 @@ plugins {
     id("org.jetbrains.kotlin.multiplatform") version "2.0.10"
 //    id("org.jetbrains.plugin.compose") version "1.7.1"
 
-
+    //https://imperceptiblethoughts.com/shadow/getting-started/
     id("com.github.johnrengelman.shadow") version "7.1.0"
-
-
-//    id("com.squareup.sqldelight") version "1.5.5"
 
     id("com.google.protobuf") version "0.9.4"
 }
-
-
-//sqldelight {
-//    database("NavigationDb") {
-//        packageName = "ca.stefanm.e39.navigation.db"
-//        //sourceFolders = listOf("db")
-//        schemaOutputDirectory = file("build/dbs")
-//        //dependency(project(":OtherProject"))
-//        dialect = "sqlite:3.24"
-//        verifyMigrations = true
-//    }
-//}
 
 kapt {
     correctErrorTypes = true
 }
 
-
-//https://imperceptiblethoughts.com/shadow/getting-started/
-
-//group "ca.stefanm"
-//version "1.0-SNAPSHOT"
-
-//sourceCompatibility = 1.8
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.time.ExperimentalTime"
@@ -68,110 +50,8 @@ repositories {
 }
 
 
-
-
-dependencies {
-
-
-    //https://github.com/gradle/kotlin-dsl-samples/issues/1372#issuecomment-515285784
-    configurations.get("kapt").dependencies.add(project(":autoDiscovery"))
-    //    kapt(project(":autoDiscovery"))
-
-    implementation(project(":autoDiscovery"))
-    implementation(project(":autoDiscoveryAnnotations"))
-
-    implementation("com.squareup.okio:okio:3.4.0")
-
-    println(System.getenv("BUILD_PI"))
-    if (System.getenv("BUILD_PI") == "true") {
-        implementation(compose.desktop.linux_arm64)
-    } else {
-        implementation(compose.desktop.currentOs)
-    }
-    implementation( "org.jetbrains.kotlin:kotlin-stdlib")
-    implementation( "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
-
-    implementation( "com.github.hypfvieh:dbus-java-osgi:3.2.3")
-    implementation( "com.github.hypfvieh:bluez-dbus:0.1.3")
-
-//    implementation("com.fazecast:jSerialComm:[2.0.0,3.0.0)")
-    implementation("com.fazecast:jSerialComm:2.10.1")
-
-
-    implementation("com.google.protobuf:protobuf-java:4.28.2")
-    implementation("com.google.protobuf:protobuf-kotlin:4.28.2")
-    implementation("com.google.protobuf:protobuf-java-util:3.23.0")
-
-    implementation("com.javadocmd:simplelatlng:1.3.1")
-    implementation("org.jxmapviewer:jxmapviewer2:2.5")
-
-    implementation("com.uchuhimo:konf:1.1.2")
-    implementation("commons-io:commons-io:2.14.0")
-
-    implementation("com.ginsberg:cirkle:1.0.1")
-
-    implementation("com.google.dagger:dagger:2.45")
-    configurations.get("kapt").dependencies.add(DefaultExternalModuleDependency("com.google.dagger:dagger-compiler:2.45"))
-//    kapt("com.google.dagger:dagger-compiler:2.45")
-
-    implementation("com.pi4j:pi4j-core:1.1")
-
-    implementation("com.jakewharton.timber:timber:4.7.1")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3")
-    testImplementation("junit:junit:4.13.1")
-
-
-//    implementation("com.squareup.sqldelight:sqlite-driver:1.5.5")
-//    implementation("com.squareup.sqldelight:coroutines-extensions-jvm:1.5.5")
-
-    //Web
-    val ktor_version = "2.1.0"
-    implementation("io.ktor:ktor:${ktor_version}")
-    implementation("io.ktor:ktor-server-netty:${ktor_version}")
-
-    implementation("io.ktor:ktor-client-core:${ktor_version}")
-    implementation("io.ktor:ktor-client-cio:${ktor_version}")
-
-    implementation("ch.qos.logback:logback-classic:1.4.12")
-    implementation("io.ktor:ktor-client-logging:${ktor_version}")
-
-
-
-    //Trixnity (https://trixnity.gitlab.io/trixnity/docs/client/create)
-    val trixnityVersion = "4.9.2"
-
-    fun trixnity(module: String, version: String = trixnityVersion) =
-        "net.folivo:trixnity-$module:$version"
-
-    implementation(trixnity("client"))
-    implementation(trixnity("client-repository-exposed"))
-    implementation(trixnity("client-media-okio"))
-
-}
-
 tasks.withType<Jar> {
     manifest {
         attributes["Main-Class"] = "ca.stefanm.ComposeMain"
-    }
-}
-compose.desktop {
-    application {
-        mainClass = "ca.stefanm.ComposeMain"
-
-
-        nativeDistributions {
-            packageVersion = "1.0.0"
-            packageName = "e39Rpi"
-            description = "BMW E39 HMI"
-            copyright = "© 2021-2025 Stefan Martynkiw."
-            vendor = "stefanm.ca"
-            targetFormats(
-                org.jetbrains.compose.desktop.application.dsl.TargetFormat.Deb
-            )
-            includeAllModules = true
-            //appResourcesRootDir.set(project.layout.projectDirectory.dir("resources"))
-
-        }
     }
 }
---
+++ src/build.gradle.kts
@@ -0,0 +1,115 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+kotlin {
+    jvm("desktop")
+
+    sourceSets {
+        val desktopMain by getting
+
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.viewmodel)
+            implementation(libs.androidx.lifecycle.runtime.compose)
+        }
+        desktopMain.dependencies {
+            //https://github.com/gradle/kotlin-dsl-samples/issues/1372#issuecomment-515285784
+//            configurations.get("kapt").dependencies.add(project(":autoDiscovery"))
+            kapt(project(":autoDiscovery"))
+            implementation(project(":autoDiscovery"))
+            implementation(project(":autoDiscoveryAnnotations"))
+
+            implementation("com.google.dagger:dagger:2.45")
+//            configurations.get("kapt").dependencies.add(DefaultExternalModuleDependency("com.google.dagger:dagger-compiler:2.45"))
+            kapt("com.google.dagger:dagger-compiler:2.45")
+
+
+
+
+            //implementation(compose.desktop.currentOs)
+            println(System.getenv("BUILD_PI"))
+            if (System.getenv("BUILD_PI") == "true") {
+                implementation(compose.desktop.linux_arm64)
+            } else {
+                implementation(compose.desktop.currentOs)
+            }
+
+            implementation(libs.kotlinx.coroutines.swing)
+//            implementation( "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+
+            implementation("commons-io:commons-io:2.14.0")
+
+            implementation("com.squareup.okio:okio:3.4.0")
+            implementation( "com.github.hypfvieh:dbus-java-osgi:3.2.3")
+            implementation( "com.github.hypfvieh:bluez-dbus:0.1.3")
+
+            implementation("com.fazecast:jSerialComm:2.10.1")
+
+            implementation("com.pi4j:pi4j-core:1.1")
+
+            implementation("com.google.protobuf:protobuf-java:4.28.2")
+            implementation("com.google.protobuf:protobuf-kotlin:4.28.2")
+            implementation("com.google.protobuf:protobuf-java-util:3.23.0")
+
+            implementation("com.javadocmd:simplelatlng:1.3.1")
+            implementation("org.jxmapviewer:jxmapviewer2:2.5")
+
+            implementation("com.ginsberg:cirkle:1.0.1")
+
+            implementation("com.uchuhimo:konf:1.1.2")
+
+            //Web
+            val ktor_version = "2.1.0"
+            implementation("io.ktor:ktor:${ktor_version}")
+            implementation("io.ktor:ktor-server-netty:${ktor_version}")
+
+            implementation("io.ktor:ktor-client-core:${ktor_version}")
+            implementation("io.ktor:ktor-client-cio:${ktor_version}")
+
+            implementation("ch.qos.logback:logback-classic:1.4.12")
+            implementation("io.ktor:ktor-client-logging:${ktor_version}")
+
+            //Trixnity (https://trixnity.gitlab.io/trixnity/docs/client/create)
+            val trixnityVersion = "4.9.2"
+
+            fun trixnity(module: String, version: String = trixnityVersion) =
+                "net.folivo:trixnity-$module:$version"
+
+            implementation(trixnity("client"))
+            implementation(trixnity("client-repository-exposed"))
+            implementation(trixnity("client-media-okio"))
+
+
+            testImplementation("junit:junit:4.13.1")
+        }
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "ca.stefanm.ComposeMain"
+        nativeDistributions {
+            packageVersion = "1.0.0"
+            packageName = "e39Rpi"
+            description = "BMW E39 HMI"
+            copyright = "© 2021-2025 Stefan Martynkiw."
+            vendor = "stefanm.ca"
+            targetFormats(
+                org.jetbrains.compose.desktop.application.dsl.TargetFormat.Deb
+            )
+            includeAllModules = true
+            //appResourcesRootDir.set(project.layout.projectDirectory.dir("resources"))
+
+        }
+    }
+}
