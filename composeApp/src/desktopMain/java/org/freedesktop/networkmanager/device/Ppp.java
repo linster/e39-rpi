f6639f8684ad086e23e84de116d26605e3d72f55
Stefan Martynkiw <linster247@gmail.com>
2026-03-28 14:13:22 -0400

    Use the old codegen for nm dbus

---
+++ composeApp/src/desktopMain/java/org/freedesktop/networkmanager/device/Ppp.java
@@ -0,0 +1,12 @@
+package org.freedesktop.networkmanager.device;
+
+import org.freedesktop.dbus.annotations.DBusInterfaceName;
+import org.freedesktop.dbus.interfaces.DBusInterface;
+
+/**
+ * Auto-generated class.
+ */
+@DBusInterfaceName("org.freedesktop.NetworkManager.Device.Ppp")
+public interface Ppp extends DBusInterface {
+
+}
