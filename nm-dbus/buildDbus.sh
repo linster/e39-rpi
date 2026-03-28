c3c8f31f9e74078f276b94f2466a30c85860f21f
Stefan Martynkiw <linster247@gmail.com>
2026-03-28 19:28:39 -0400

    tin-bash the generator to not be so fussy

--- nm-dbus/buildDbus.sh
+++ nm-dbus/buildDbus.sh
@@ -2,14 +2,17 @@
 
 # Meant to be run inside the docker container
 
-git clone -b dbus-java-parent-5.2.0 https://github.com/hypfvieh/dbus-java.git
-git clone https://github.com/NetworkManager/NetworkManager.git
+git clone -b remove-check https://github.com/linster/dbus-java.git
+git clone --depth 1 https://github.com/NetworkManager/NetworkManager.git
 
 cd /network-manager/dbus-java || exit
 mvn clean install -DskipTests=true
 
 # then, run the builder script
 cd /network-manager/dbus-java/dbus-java-utils || exit
+
+
+
 
 find /network-manager/NetworkManager/introspection/*.xml -type f -exec mvn exec:java \
 -Dexec.mainClass="org.freedesktop.dbus.utils.generator.InterfaceCodeGenerator" \
