dee1e27c74c9ed265d1b4bb7114e074c0dac3707
Stefan Martynkiw <linster247@gmail.com>
2026-03-29 19:35:03 -0400

    Activating Connection Dialog has a throbber now :)

--- composeApp/src/desktopMain/kotlin/ca/stefanm/ibus/gui/networkSetup/ui/ActivateConnectionScreen.kt
+++ composeApp/src/desktopMain/kotlin/ca/stefanm/ibus/gui/networkSetup/ui/ActivateConnectionScreen.kt
@@ -1,16 +1,24 @@
 package ca.stefanm.ca.stefanm.ibus.gui.networkSetup.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import ca.stefanm.ibus.annotations.screenflow.ScreenDoc
 import ca.stefanm.ibus.autoDiscover.AutoDiscover
+import ca.stefanm.ibus.gui.menu.Notification
 import ca.stefanm.ibus.gui.menu.navigator.NavigationNode
 import ca.stefanm.ibus.gui.menu.navigator.Navigator
 import ca.stefanm.ibus.gui.menu.notifications.NotificationHub
+import ca.stefanm.ibus.gui.menu.widgets.BmwSingleLineHeader
 import ca.stefanm.ibus.gui.menu.widgets.modalMenu.ModalMenuService
+import ca.stefanm.ibus.gui.menu.widgets.screenMenu.FullScreenMenu
+import ca.stefanm.ibus.gui.menu.widgets.screenMenu.TextMenuItem
 import ca.stefanm.ibus.lib.logging.Logger
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.seconds
 
 @ScreenDoc(
     screenName = "ActivateConnectionScreen",
@@ -28,6 +36,35 @@ class ActivateConnectionScreen @Inject constructor(
 
     override fun provideMainContent(): @Composable ((Navigator.IncomingResult?) -> Unit) = {
 
+        Column(Modifier.fillMaxSize()) {
+            BmwSingleLineHeader("Activate Connection")
+
+            FullScreenMenu.OneColumn(
+                listOf(
+                    TextMenuItem(
+                        title = "Go Back",
+                        onClicked = {}
+                    ),
+                    TextMenuItem(
+                        title = "Show throbber",
+                        onClicked = {
+                            showConnectingThrobber()
+                        }
+                    ),
+                )
+            )
+        }
+    }
+
+    fun showConnectingThrobber() {
+        modalMenuService.showModalWaitDialog(
+            image = Notification.NotificationImage.NONE,
+            throbber = true,
+            headerText = "Connecting...",
+            bodyText = "Activating Connection",
+            isCancellable = false,
+            autoCloseTimeout = 3.seconds
+        )
     }
 
 }
